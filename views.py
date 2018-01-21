943e8f29b5aeeeecb318786276d94fd71a6e0c0b
wangzheng62 <363579018@qq.com>
2018-01-21 18:34:35 +0800

    111

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
 from flask import Flask,redirect,url_for,render_template,request,flash
-from func import DBserver,Crm,Product
+from func import DBserver,Crm,Product,Orderlist,Employee,Customer
 
 app = Flask(__name__)
 app.secret_key = 'some_secret'
@@ -19,7 +19,28 @@ def prouductmain():
 @app.route('/employeemain')
 def employeemain():
     return render_template('employeemain.html')
+#客户页功能
+@app.route('/customeradd')
+def customeradd():
+    res=Customer.desc()
+    print(res)
+    return render_template('customermain.html',addform=res,bindurl="addcustomer")
+@app.route('/addcustomer',methods=["post"])
+def addcustomer():
+    res=request.form.to_dict()
+    print(res)
+    res["__employee_Id"]="123"
+    l=Customer(**res)
+    l.save()
+    return  render_template('customermain.html')
+@app.route('/customersearch')
+def customersearch():
+    res=Customer.fetchall()
+    res.insert(0,Customer.colnames())
+    return render_template('customermain.html',addlist=res)
 
+
+    return  render_template('customermain.html')
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
 
