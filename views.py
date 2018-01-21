cc98fb9a856846753275b1d7ad1648937dc449c6
wangzheng62 <363579018@qq.com>
2018-01-21 17:07:13 +0800

    111

--- views.py
+++ views.py
@@ -19,26 +19,6 @@ def prouductmain():
 @app.route('/employeemain')
 def employeemain():
     return render_template('employeemain.html')
-@app.route('/getform',methods=["get","post"])
-def getform():
-    a=request.form
-    l=Product(**a)
-    print(l.info)
-    if l.save():
-        flash("提交成功")
-    else:
-        flash("提交失败")
-    return redirect(url_for("form01"))
-@app.route('/search',methods=["get","post"])
-def search():
-    a=request.form
-    l=Product(**a)
-    res=l.search()
-    names=l.colnames()
-    if res==[]:
-        flash('没有查到结果')
-    res.insert(0,names)
-    return render_template('search01.html',formdata=names, listdata=res)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True)
